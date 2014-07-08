720ca52a28e405d33c16d923495754418ce65e22
Vishal Anand <vishal544@hotmail.com>
2014-07-08 21:40:37 +0000

    sending mail

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ActionMailer::Base
+  default from: "do-not-relpy@example.com"
+  
+  def contact_email(contact)
+    @contact = contact
+    mail(to: Rails.application.secrets.owner_email, from: @contact.email, :subject => "Website Contact")
+  end
+end
